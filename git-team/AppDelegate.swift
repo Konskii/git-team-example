d6a60e8b24b7e2d2fcdaf118e78b1437eb7e99cc
Skripkin <artem.skripkin@magenta-technology.com>
2023-06-03 21:56:50 +0400

    test2

--- git-team/AppDelegate.swift
+++ git-team/AppDelegate.swift
@@ -10,6 +10,12 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    func two() {
+        print("one")
+        print(2 * 2)
+    }
+
+
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
