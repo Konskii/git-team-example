0098335a78c13be3d8f234bb95a8fbfc03b2790c
Skripkin <artem.skripkin@magenta-technology.com>
2023-06-03 21:19:18 +0400

    initial create screens in storyboard

---
+++ git-team/LoginViewController.swift
@@ -0,0 +1,12 @@
+//
+//  LoginViewController.swift
+//  git-team
+//
+//  Created by Артём Скрипкин on 03.06.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+}
---
+++ git-team/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  git-team
+//
+//  Created by Артём Скрипкин on 03.06.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+}
